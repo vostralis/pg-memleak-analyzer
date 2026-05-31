b5c0f0de0bc28b9bd93b7ab4555478bac350ad52
Timofey Tagaev <vostralis@proton.me>
2026-05-31 19:43:50 +0700

    feat: start custom query execution pipeline implementation - start analyze_query implementation and add corresponding sql wrapper - change ContextNode signature and update reset_snapshot procedure accordingly - add backend_profiling_active flag to allow standard query execution

--- memleak_analyzer--1.0.sql
+++ memleak_analyzer--1.0.sql
@@ -3,3 +3,16 @@
 \echo Use "CREATE EXTENSION memleak_analyzer" to load this file. \quit
 
 CREATE SCHEMA IF NOT EXISTS memleak_analyzer;
+
+CREATE OR REPLACE FUNCTION memleak_analyzer.analyze_query(query text)
+RETURNS TABLE (
+    context_name     TEXT,
+    parent_name      TEXT,
+    level            INT,
+    allocated_before BIGINT,
+    allocated_after  BIGINT,
+    delta_bytes      BIGINT,
+    leak_suspected   BOOLEAN
+)
+AS 'MODULE_PATHNAME', 'analyze_query'
+LANGUAGE C STRICT VOLATILE;
--- memleak_analyzer.c
+++ memleak_analyzer.c
@@ -1,27 +1,33 @@
 #include "postgres.h"
 #include "fmgr.h"
+#include "funcapi.h"
 
 #include "executor/executor.h"
+#include "executor/spi.h"
+#include "utils/builtins.h"
 
 PG_MODULE_MAGIC;
 
-typedef struct ContextNode {
-    char   name[64];
-    int    level;
-    uint64 total_bytes;
-    uint64 free_bytes;
-} ContextNode;
+/* Constants */
+#define CONTEXT_NAME_MAX_LEN 64
+#define SNAPSHOT_MAX_NODES   512
 
-#define SNAPSHOT_MAX_NODES 256
+typedef struct ContextNode {
+    char   name[CONTEXT_NAME_MAX_LEN];
+    char   parent_name[CONTEXT_NAME_MAX_LEN];
+    int    level;
+    uint64 used_bytes;
+} ContextNode;
 
 typedef struct MemorySnapshot {
     ContextNode nodes[SNAPSHOT_MAX_NODES];
     int         node_count;
 } MemorySnapshot;
 
+static bool backend_profiling_active = false;
 /* Snapshots for capturing the state of memory before and after a client request is executed */
-static MemorySnapshot snapshot_before;
-static MemorySnapshot snapshot_after;
+static MemorySnapshot backend_snapshot_before;
+static MemorySnapshot backend_snapshot_after;
 
 /* Saved hook values in case of unload */
 static ExecutorStart_hook_type prev_ExecutorStart = NULL;
@@ -34,10 +40,20 @@ static void analyzer_ExecutorStart(QueryDesc *queryDesc, int eflags);
 static void analyzer_ExecutorEnd(QueryDesc *queryDesc);
 static void reset_snapshot(MemorySnapshot *snapshot);
 
+/* SQL binds */
+Datum analyze_query(PG_FUNCTION_ARGS);
+
 static void
 analyzer_ExecutorStart(QueryDesc *queryDesc, int eflags)
 {
     Assert(queryDesc != NULL);
+
+    if (backend_profiling_active)
+    {
+        elog(LOG, "Profiling is active (start)");
+
+        /* Do something */
+    }
 
     if (prev_ExecutorStart)
         prev_ExecutorStart(queryDesc, eflags);
@@ -49,6 +65,13 @@ static void
 analyzer_ExecutorEnd(QueryDesc *queryDesc)
 {
     Assert(queryDesc != NULL);
+
+    if (backend_profiling_active)
+    {
+        elog(LOG, "Profiling is active (end)");
+
+        /* Do something */
+    }
 
     if (prev_ExecutorEnd)
         prev_ExecutorEnd(queryDesc);
@@ -64,15 +87,58 @@ reset_snapshot(MemorySnapshot *snapshot)
     for (size_t i = 0; i < snapshot->node_count; ++i) {
         ContextNode *node = &snapshot->nodes[i];
 
-        memset(node->name, 0, sizeof(node->name));
+        memset(node->name, 0, CONTEXT_NAME_MAX_LEN);
+        memset(node->parent_name, 0, CONTEXT_NAME_MAX_LEN);
         node->level = 0;
-        node->total_bytes = 0;
-        node->free_bytes = 0;
+        node->used_bytes = 0;
     }
 
     snapshot->node_count = 0;
 }
 
+PG_FUNCTION_INFO_V1(analyze_query);
+
+Datum
+analyze_query(PG_FUNCTION_ARGS)
+{
+    char *query = text_to_cstring(PG_GETARG_TEXT_PP(0)); /* Extract target query */
+    
+    InitMaterializedSRF(fcinfo, 0);
+
+    PG_TRY();
+    {
+        backend_profiling_active = true; /* Enable profiling flag */
+        /* Reset memory snapshots before a query execution */
+        reset_snapshot(&backend_snapshot_before);
+        reset_snapshot(&backend_snapshot_after);
+
+        SPI_connect();
+        
+        if (SPI_execute(query, false, 0) < 0)
+        {
+            /* Query execution failure */
+            ereport(
+                ERROR,
+                errcode(ERRCODE_SYNTAX_ERROR),
+                errmsg("Failed to execute query: %s", query)
+            );
+        }
+        
+        SPI_finish();
+
+        backend_profiling_active = false;
+    }
+    PG_CATCH();
+    {
+        SPI_finish();
+        backend_profiling_active = false;
+        
+        PG_RE_THROW();
+    }
+    PG_END_TRY();
+
+    PG_RETURN_NULL();
+}
 
 void
 _PG_init(void)
